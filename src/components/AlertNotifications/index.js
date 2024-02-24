7d8ad750a43db6be56cf26b2401bb8d1f550ff34
Sallonii <121022481+Sallonii@users.noreply.github.com>
2024-02-24 11:31:15 +0530

    Update index.js

--- src/components/AlertNotifications/index.js
+++ src/components/AlertNotifications/index.js
@@ -1,5 +1,4 @@
 import {AiFillCheckCircle} from 'react-icons/ai'
-import {GrFormClose} from 'react-icons/gr'
 import {RiErrorWarningFill} from 'react-icons/ri'
 import {MdWarning, MdInfo} from 'react-icons/md'
 
@@ -10,52 +9,40 @@ const AlertNotifications = () => (
   <div className="bg">
     <h1>Alert Notifications</h1>
     <Notification>
-      <div>
-        <div className="icon-heading-container">
-          <AiFillCheckCircle className="success-icon" />
-          <h1 className="success-heading">Success</h1>
-        </div>
-        <p className="success-description">
-          You can access all the files in the folder
-        </p>
+      <div className="icon-heading-container">
+        <AiFillCheckCircle className="success-icon" />
+        <h1 className="success-heading">Success</h1>
       </div>
-      <GrFormClose />
+      <p className="success-description">
+        You can access all the files in the folder
+      </p>
     </Notification>
     <Notification>
-      <div>
-        <div className="icon-heading-container">
-          <RiErrorWarningFill className="error-icon" />
-          <h1 className="error-heading">Error</h1>
-        </div>
-        <p className="success-description">
-          Sorry, you are not authorized to have access to delete the file
-        </p>
+      <div className="icon-heading-container">
+        <RiErrorWarningFill className="error-icon" />
+        <h1 className="error-heading">Error</h1>
       </div>
-      <GrFormClose />
+      <p className="success-description">
+        Sorry, you are not authorized to have access to delete the file
+      </p>
     </Notification>
     <Notification>
-      <div>
-        <div className="icon-heading-container">
-          <MdWarning className="warning-icon" />
-          <h1 className="warning-heading">Warning</h1>
-        </div>
-        <p className="success-description">
-          Viewers of this file can see comments and suggestions
-        </p>
+      <div className="icon-heading-container">
+        <MdWarning className="warning-icon" />
+        <h1 className="warning-heading">Warning</h1>
       </div>
-      <GrFormClose />
+      <p className="success-description">
+        Viewers of this file can see comments and suggestions
+      </p>
     </Notification>
     <Notification>
-      <div>
-        <div className="icon-heading-container">
-          <MdInfo className="info-icon" />
-          <h1 className="info-heading">Info</h1>
-        </div>
-        <p className="success-description">
-          Anyone on the internet can view these files
-        </p>
+      <div className="icon-heading-container">
+        <MdInfo className="info-icon" />
+        <h1 className="info-heading">Info</h1>
       </div>
-      <GrFormClose />
+      <p className="success-description">
+        Anyone on the internet can view these files
+      </p>
     </Notification>
   </div>
 )
